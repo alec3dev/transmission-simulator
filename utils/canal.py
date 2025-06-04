0ea94006db4a0d4700eeaf340d16f4a3074d142b
alec3dev <82599056+alec3dev@users.noreply.github.com>
2025-06-05 00:06:23 +0100

    Update canal.py

--- utils/canal.py
+++ utils/canal.py
@@ -1,1 +1,8 @@
+import numpy as np
 
+def ajout_bruit(signal, snr_db):
+    """Ajoute un bruit gaussien au signal selon le SNR spécifié"""
+    puissance_signal = np.mean(signal**2)
+    puissance_bruit = puissance_signal / (10 ** (snr_db / 10))
+    bruit = np.random.normal(0, np.sqrt(puissance_bruit), len(signal))
+    return signal + bruit
